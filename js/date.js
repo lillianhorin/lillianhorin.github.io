acb15ee5d30e1eeeaaa670a01980f2f5bee13a49
Lillian Horin <lillianhorin@gmail.com>
2023-01-03 14:02:06 -0500

    Change date placement

--- js/date.js
+++ js/date.js
@@ -1,5 +1,2 @@
 // Append year to copyright
 document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-
-// Append current date to updated section
-document.getElementById("update").appendChild(document.createTextNode( "January 2, 2023"));
