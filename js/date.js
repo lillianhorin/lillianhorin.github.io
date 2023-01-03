a80d479b42587d376a9443e99aef4f7570f4fe41
Lillian Horin <lillianhorin@gmail.com>
2023-01-03 00:26:47 -0500

    Update date.js

--- js/date.js
+++ js/date.js
@@ -2,8 +2,4 @@
 document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
 
 // Append current date to updated section
-date = new Date();
-year = date.getFullYear();
-month = date.getMonth() + 1;
-day = date.getDate();
-document.getElementById("update").appendChild(document.createTextNode( month + "/" + day + "/" + year ));
+document.getElementById("update").appendChild(document.createTextNode( "January 2, 2023"));
